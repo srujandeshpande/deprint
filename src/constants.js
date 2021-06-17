3fb52c736e4b26d9de909275a134fdaf3374fd9a
Srujan Deshpande <srujan.deshpande@gmail.com>
2021-06-17 22:29:09 +0530

    Convert tabs to spaces

--- src/constants.js
+++ src/constants.js
@@ -1,23 +1,23 @@
 const langs = {
-    js: ['javascript', 'javascriptreact', 'typescript', 'typescriptreact'],
-    py: ['python'],
-    hash: ['python'],
-    slash: ['javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'c', 'c++']
+  js: ['javascript', 'javascriptreact', 'typescript', 'typescriptreact'],
+  py: ['python'],
+  hash: ['python'],
+  slash: ['javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'c', 'c++']
 };
 
 const patterns = {
-    py: {
-        print: "^[ \t]*print[ \t]*\(.*\)",
-        printComment: "^[ \t]*#[ \t]*print[ \t]*\(.*\)"
-    },
-    js: {
-        log: "^[ \t]*console\.log[ \t]*\(.*\)",
-        logComment: "^[ \t]*\/\/[ \t]*console\.log[ \t]*\(.*\)"
-    },
-    comment: {
-        hash: "^[ \t]*#.*",
-        slash: "^[ \t]*\/\/.*"
-    }
+  py: {
+    print: "^[ \t]*print[ \t]*\(.*\)",
+    printComment: "^[ \t]*#[ \t]*print[ \t]*\(.*\)"
+  },
+  js: {
+    log: "^[ \t]*console\.log[ \t]*\(.*\)",
+    logComment: "^[ \t]*\/\/[ \t]*console\.log[ \t]*\(.*\)"
+  },
+  comment: {
+    hash: "^[ \t]*#.*",
+    slash: "^[ \t]*\/\/.*"
+  }
 };
 
 module.exports = { langs, patterns };
--- src/extension.ts
+++ src/extension.ts
@@ -5,169 +5,169 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "deprint" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "deprint" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let commentPrint = vscode.commands.registerCommand('deprint.comment.print', () => {
-		// The code you place here will be executed every time your command is executed
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let commentPrint = vscode.commands.registerCommand('deprint.comment.print', () => {
+    // The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Commenting print and log statements!');
+    // Display a message box to the user
+    vscode.window.showInformationMessage('Commenting print and log statements!');
 
-		const textEditor = vscode.window.activeTextEditor;
-		if (!textEditor) {
-			return;  // No open text editor
-		}
+    const textEditor = vscode.window.activeTextEditor;
+    if (!textEditor) {
+      return;  // No open text editor
+    }
 
-		let filename = textEditor.document.fileName;
-		let langId = textEditor.document.languageId;
+    let filename = textEditor.document.fileName;
+    let langId = textEditor.document.languageId;
 
-		if (langs.js.includes(langId)) {
-			// JS Stuff console.log()
-			textEditor.edit(function (editBuilder) {
-				// Iterate through each line
-				for (var i = 0; i < textEditor.document.lineCount; i++) {
-					let linetext = textEditor.document.lineAt(i).text;
-					var newline = linetext;
+    if (langs.js.includes(langId)) {
+      // JS Stuff console.log()
+      textEditor.edit(function (editBuilder) {
+        // Iterate through each line
+        for (var i = 0; i < textEditor.document.lineCount; i++) {
+          let linetext = textEditor.document.lineAt(i).text;
+          var newline = linetext;
 
-					if (linetext.match(patterns.js.log)) {
-						var j = 0;
-						for (j = 0; j < linetext.length; j++) {
-							if (linetext[j] != ' ' && linetext[j] != '\t') break;
-						}
-						newline = [linetext.slice(0, j), "// ", linetext.slice(j)].join('');
-					}
-					var textRange = new vscode.Range(i, textEditor.document.lineAt(i).range.start.character, i, textEditor.document.lineAt(i).range.end.character);
-					editBuilder.replace(textRange, newline);
-				}
-			});
-		}
-		else if (langs.py.includes(langId)) {
-			// Python Stuff print()
-			textEditor.edit(function (editBuilder) {
-				// Iterate through each line
-				for (var i = 0; i < textEditor.document.lineCount; i++) {
-					let linetext = textEditor.document.lineAt(i).text;
-					var newline = linetext;
+          if (linetext.match(patterns.js.log)) {
+            var j = 0;
+            for (j = 0; j < linetext.length; j++) {
+              if (linetext[j] != ' ' && linetext[j] != '\t') break;
+            }
+            newline = [linetext.slice(0, j), "// ", linetext.slice(j)].join('');
+          }
+          var textRange = new vscode.Range(i, textEditor.document.lineAt(i).range.start.character, i, textEditor.document.lineAt(i).range.end.character);
+          editBuilder.replace(textRange, newline);
+        }
+      });
+    }
+    else if (langs.py.includes(langId)) {
+      // Python Stuff print()
+      textEditor.edit(function (editBuilder) {
+        // Iterate through each line
+        for (var i = 0; i < textEditor.document.lineCount; i++) {
+          let linetext = textEditor.document.lineAt(i).text;
+          var newline = linetext;
 
-					if (linetext.match(patterns.py.print)) {
-						var j = 0;
-						for (j = 0; j < linetext.length; j++) {
-							if (linetext[j] != ' ' && linetext[j] != '\t') break;
-						}
-						newline = [linetext.slice(0, j), "# ", linetext.slice(j)].join('');
-					}
-					var textRange = new vscode.Range(i, textEditor.document.lineAt(i).range.start.character, i, textEditor.document.lineAt(i).range.end.character);
-					editBuilder.replace(textRange, newline);
-				}
-			});
-		}
-		else {
-			return;
-		}
+          if (linetext.match(patterns.py.print)) {
+            var j = 0;
+            for (j = 0; j < linetext.length; j++) {
+              if (linetext[j] != ' ' && linetext[j] != '\t') break;
+            }
+            newline = [linetext.slice(0, j), "# ", linetext.slice(j)].join('');
+          }
+          var textRange = new vscode.Range(i, textEditor.document.lineAt(i).range.start.character, i, textEditor.document.lineAt(i).range.end.character);
+          editBuilder.replace(textRange, newline);
+        }
+      });
+    }
+    else {
+      return;
+    }
 
-	});
-	context.subscriptions.push(commentPrint);
-
-
-	let deleteCommentedPrint = vscode.commands.registerCommand('deprint.delete.comment.print', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Deleting commented print statements');
-
-		const textEditor = vscode.window.activeTextEditor;
-		if (!textEditor) {
-			return;  // No open text editor
-		}
-
-		let filename = textEditor.document.fileName;
-		let langId = textEditor.document.languageId;
-
-		if (langs.js.includes(langId)) {
-			// JS Stuff console.log()
-			textEditor.edit(function (editBuilder) {
-				// Iterate through each line
-				for (var i = 0; i < textEditor.document.lineCount; i++) {
-					let linetext = textEditor.document.lineAt(i).text;
-
-					if (linetext.match(patterns.js.logComment)) {
-						var textRange = new vscode.Range(i, textEditor.document.lineAt(i).range.start.character, i + 1, textEditor.document.lineAt(i + 1).range.start.character);
-						editBuilder.replace(textRange, "");
-					}
-				}
-			});
-		}
-		else if (langs.py.includes(langId)) {
-			// Python Stuff print()
-			textEditor.edit(function (editBuilder) {
-				// Iterate through each line
-				for (var i = 0; i < textEditor.document.lineCount; i++) {
-					let linetext = textEditor.document.lineAt(i).text;
-
-					if (linetext.match(patterns.py.printComment)) {
-						var textRange = new vscode.Range(i, textEditor.document.lineAt(i).range.start.character, i + 1, textEditor.document.lineAt(i + 1).range.start.character);
-						editBuilder.replace(textRange, "");
-					}
-				}
-			});
-		}
-		else {
-			return;
-		}
+  });
+  context.subscriptions.push(commentPrint);
 
 
-	});
-	context.subscriptions.push(deleteCommentedPrint);
+  let deleteCommentedPrint = vscode.commands.registerCommand('deprint.delete.comment.print', () => {
+    // The code you place here will be executed every time your command is executed
+
+    // Display a message box to the user
+    vscode.window.showInformationMessage('Deleting commented print statements');
+
+    const textEditor = vscode.window.activeTextEditor;
+    if (!textEditor) {
+      return;  // No open text editor
+    }
+
+    let filename = textEditor.document.fileName;
+    let langId = textEditor.document.languageId;
+
+    if (langs.js.includes(langId)) {
+      // JS Stuff console.log()
+      textEditor.edit(function (editBuilder) {
+        // Iterate through each line
+        for (var i = 0; i < textEditor.document.lineCount; i++) {
+          let linetext = textEditor.document.lineAt(i).text;
+
+          if (linetext.match(patterns.js.logComment)) {
+            var textRange = new vscode.Range(i, textEditor.document.lineAt(i).range.start.character, i + 1, textEditor.document.lineAt(i + 1).range.start.character);
+            editBuilder.replace(textRange, "");
+          }
+        }
+      });
+    }
+    else if (langs.py.includes(langId)) {
+      // Python Stuff print()
+      textEditor.edit(function (editBuilder) {
+        // Iterate through each line
+        for (var i = 0; i < textEditor.document.lineCount; i++) {
+          let linetext = textEditor.document.lineAt(i).text;
+
+          if (linetext.match(patterns.py.printComment)) {
+            var textRange = new vscode.Range(i, textEditor.document.lineAt(i).range.start.character, i + 1, textEditor.document.lineAt(i + 1).range.start.character);
+            editBuilder.replace(textRange, "");
+          }
+        }
+      });
+    }
+    else {
+      return;
+    }
 
 
-	let deleteComment = vscode.commands.registerCommand('deprint.delete.comment', () => {
+  });
+  context.subscriptions.push(deleteCommentedPrint);
 
-		vscode.window.showInformationMessage('Deleting all comments');
 
-		const textEditor = vscode.window.activeTextEditor;
-		if (!textEditor) {
-			return;  // No open text editor
-		}
+  let deleteComment = vscode.commands.registerCommand('deprint.delete.comment', () => {
 
-		let langId = textEditor.document.languageId;
+    vscode.window.showInformationMessage('Deleting all comments');
 
-		if (langs.hash.includes(langId)) {
-			textEditor.edit(function (editBuilder) {
-				// Iterate through each line
-				for (var i = 0; i < textEditor.document.lineCount; i++) {
-					let linetext = textEditor.document.lineAt(i).text;
+    const textEditor = vscode.window.activeTextEditor;
+    if (!textEditor) {
+      return;  // No open text editor
+    }
 
-					if (linetext.match(patterns.comment.hash)) {
-						var textRange = new vscode.Range(i, textEditor.document.lineAt(i).range.start.character, i + 1, textEditor.document.lineAt(i + 1).range.start.character);
-						editBuilder.replace(textRange, "");
-					}
-				}
-			});
-		}
-		else if (langs.slash.includes(langId)) {
-			textEditor.edit(function (editBuilder) {
-				// Iterate through each line
-				for (var i = 0; i < textEditor.document.lineCount; i++) {
-					let linetext = textEditor.document.lineAt(i).text;
+    let langId = textEditor.document.languageId;
 
-					if (linetext.match(patterns.comment.slash)) {
-						var textRange = new vscode.Range(i, textEditor.document.lineAt(i).range.start.character, i + 1, textEditor.document.lineAt(i + 1).range.start.character);
-						editBuilder.replace(textRange, "");
-					}
-				}
-			});
-		}
-		else {
-			return;
-		}
+    if (langs.hash.includes(langId)) {
+      textEditor.edit(function (editBuilder) {
+        // Iterate through each line
+        for (var i = 0; i < textEditor.document.lineCount; i++) {
+          let linetext = textEditor.document.lineAt(i).text;
 
-	});
-	context.subscriptions.push(deleteComment);
+          if (linetext.match(patterns.comment.hash)) {
+            var textRange = new vscode.Range(i, textEditor.document.lineAt(i).range.start.character, i + 1, textEditor.document.lineAt(i + 1).range.start.character);
+            editBuilder.replace(textRange, "");
+          }
+        }
+      });
+    }
+    else if (langs.slash.includes(langId)) {
+      textEditor.edit(function (editBuilder) {
+        // Iterate through each line
+        for (var i = 0; i < textEditor.document.lineCount; i++) {
+          let linetext = textEditor.document.lineAt(i).text;
+
+          if (linetext.match(patterns.comment.slash)) {
+            var textRange = new vscode.Range(i, textEditor.document.lineAt(i).range.start.character, i + 1, textEditor.document.lineAt(i + 1).range.start.character);
+            editBuilder.replace(textRange, "");
+          }
+        }
+      });
+    }
+    else {
+      return;
+    }
+
+  });
+  context.subscriptions.push(deleteComment);
 }
 
 // this method is called when your extension is deactivated
