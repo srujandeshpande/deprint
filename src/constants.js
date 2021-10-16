b47e61206dd77e5fcfa11abb4c5f21d092b6fbc7
PhoenixFlame101 <abhinavchris@gmail.com>
2021-10-16 10:56:55 +0530

    Revert "Added ruby support;"
    This reverts commit 0bdea35800148401e219c95b421421aa86009053.

--- src/constants.js
+++ src/constants.js
@@ -2,8 +2,7 @@ const langs = {
   js: ['javascript', 'javascriptreact', 'typescript', 'typescriptreact'],
   py: ['python'],
   c: ['c', 'cpp'],
-  rb: ['ruby'],
-  hash: ['python', 'ruby'],
+  hash: ['python'],
   slash: ['javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'c', 'c++', 'java']
 };
 
@@ -11,10 +10,6 @@ const patterns = {
   py: {
     print: "^[ \t]*print[ \t]*\(.*\)",
     printComment: "^[ \t]*#[ \t]*print[ \t]*\(.*\)"
-  },
-  rb: {
-    puts: "^[ \t]*puts[ \t]*$",
-    putsComment: "^[ \t]*#[ \t]*puts[ \t]*$"
   },
   js: {
     log: "^[ \t]*console\.log[ \t]*\(.*\)",
